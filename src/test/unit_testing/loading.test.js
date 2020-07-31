d32c01b59f27ce0bbb19e11cc6f7c2b18ac4c160
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2020-07-31 17:34:22 +0800

    init

--- src/test/unit_testing/loading.test.js
+++ src/test/unit_testing/loading.test.js
@@ -2,7 +2,7 @@
  * @Author: junjie.lean
  * @Date: 2020-03-18 11:09:36
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2020-03-24 13:04:09
+ * @Last Modified time: 2020-07-31 17:34:16
  */
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
@@ -11,21 +11,21 @@ import { expect } from "chai";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-import Loading from "../../view/page/layout-loading";
+// import Loading from "../../view/page/layout-loading";
 
-import { shallow, mount, render } from "enzyme";
+// import { shallow, mount, render } from "enzyme";
 
-describe("<Loading />", () => {
-  test("shallow testing:", () => {
-    const wrapper = shallow(<Loading />);
-    expect(wrapper).to.be.a("object");
-  });
-});
+// describe("<Loading />", () => {
+//   test("shallow testing:", () => {
+//     const wrapper = shallow(<Loading />);
+//     expect(wrapper).to.be.a("object");
+//   });
+// });
 
-describe("<Loading />", () => {
-  it("render testing:", () => {
-    const wrapper = mount(<Loading />);
-    expect(wrapper.find(".lean-mask")).to.have.lengthOf(1);
-    // expect(wrapper.find("button")).to.have.lengthOf(1);
-  });
-});
+// describe("<Loading />", () => {
+//   it("render testing:", () => {
+//     const wrapper = mount(<Loading />);
+//     expect(wrapper.find(".lean-mask")).to.have.lengthOf(1);
+//     // expect(wrapper.find("button")).to.have.lengthOf(1);
+//   });
+// });
